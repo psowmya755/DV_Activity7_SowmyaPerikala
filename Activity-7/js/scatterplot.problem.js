0f7aa9d2812908e8e75c9a2bc10e4cc52f014d68
psowmya755 <59047036+psowmya755@users.noreply.github.com>
2025-11-23 23:58:31 -0500

    Add files via upload

---
+++ Activity-7/js/scatterplot.problem.js
@@ -0,0 +1,178 @@
+function scatter_plot(
+    data,
+    ax,
+    title = "",
+    xCol = "",
+    yCol = "",
+    rCol = "",
+    legend = [],
+    colorCol = "",
+    margin = 50
+) {
+    const X = data.map(d => d[xCol]);
+    const Y = data.map(d => d[yCol]);
+    const R = data.map(d => d[rCol]);
+    const colorCategories = [...new Set(data.map(d => d[colorCol]))];
+
+    const color = d3.scaleOrdinal()
+        .domain(colorCategories)
+        .range(d3.schemeTableau10);
+
+    const xExtent = d3.extent(X, d => +d);
+    const yExtent = d3.extent(Y, d => +d);
+
+    const xMargin = (xExtent[1] - xExtent[0]) * 0.05;
+    const yMargin = (yExtent[1] - yExtent[0]) * 0.05;
+
+    const xScale = d3.scaleLinear()
+        .domain([xExtent[0] - xMargin, xExtent[1] + xMargin])
+        .range([margin, 1000 - margin]);
+
+    const yScale = d3.scaleLinear()
+        .domain([yExtent[0] - yMargin, yExtent[1] + yMargin])
+        .range([1000 - margin, margin]);
+
+    const rScale = d3.scaleSqrt()
+        .domain(d3.extent(R, d => +d))
+        .range([4, 12]);
+
+    const Fig = d3.select(`${ax}`);
+
+    // markers
+    Fig.selectAll(".markers")
+        .data(data)
+        .join("g")
+        .attr("transform", d => `translate(${xScale(d[xCol])}, ${yScale(d[yCol])})`)
+        .append("circle")
+        .attr("class", (d, i) => `cls_${i} ${d[colorCol]}`)
+        // id has index so we can map back into data array
+        .attr("id", (d, i) => `id_${i} ${d[colorCol]}`)
+        .attr("r", d => rScale(d[rCol]))
+        .attr("fill", d => color(d[colorCol]));
+
+    // axes
+    const x_axis = d3.axisBottom(xScale).ticks(4);
+    const y_axis = d3.axisLeft(yScale).ticks(4);
+
+    Fig.append("g")
+        .attr("class", "axis")
+        .attr("transform", `translate(0,${1000 - margin})`)
+        .call(x_axis);
+
+    Fig.append("g")
+        .attr("class", "axis")
+        .attr("transform", `translate(${margin},0)`)
+        .call(y_axis);
+
+    // labels
+    Fig.append("g")
+        .attr("class", "label")
+        .attr("transform", `translate(500,${1000 - 10})`)
+        .append("text")
+        .attr("class", "label")
+        .text(xCol)
+        .attr("fill", "black");
+
+    Fig.append("g")
+        .attr("transform", `translate(35,500) rotate(270)`)
+        .append("text")
+        .attr("class", "label")
+        .text(yCol)
+        .attr("fill", "black");
+
+    // title
+    Fig.append("text")
+        .attr("x", 500)
+        .attr("y", 80)
+        .attr("text-anchor", "middle")
+        .text(title)
+        .attr("class", "title")
+        .attr("fill", "black");
+
+    // ===== Brushing ====================================================
+    const brush = d3.brush()
+        .on("start", brushStart)
+        .on("brush end", brushed)
+        .extent([
+            [margin, margin],
+            [1000 - margin, 1000 - margin]
+        ]);
+
+    Fig.call(brush);
+
+    function brushStart() {
+        const sel = d3.brushSelection(this);
+
+        // If there is no real brush selection (user just clicked),
+        // clear all selected points.
+        ////////////////////////////////////////////////////////////////////////
+        // Missing Part 4 
+        ////////////////////////////////////////////////////////////////////////
+        if (!sel || sel[0][0] === sel[1][0]) {
+            d3.selectAll("circle").classed("selected", false);
+        }
+    }
+
+    function brushed() {
+        const selected_coordinates = d3.brushSelection(this);
+        if (!selected_coordinates) return;
+
+        const X1 = xScale.invert(selected_coordinates[0][0]);
+        const X2 = xScale.invert(selected_coordinates[1][0]);
+        const Y1 = yScale.invert(selected_coordinates[0][1]);
+        const Y2 = yScale.invert(selected_coordinates[1][1]);
+
+        d3.selectAll("circle").classed("selected", d => {
+            return (
+                +d[xCol] >= X1 &&
+                +d[xCol] <= X2 &&
+                +d[yCol] <= Y1 &&
+                +d[yCol] >= Y2
+            );
+        });
+    }
+
+    // ===== Legend with filtering ======================================
+    const legendContainer = Fig.append("g")
+        .attr("transform", `translate(${800},${margin})`)
+        .attr("class", "marginContainer");
+
+    if (legend.length === 0) {
+        legend = colorCategories;
+    }
+
+    const legends_items = legendContainer.selectAll("legends")
+        .data(legend)
+        .join("g")
+        .attr("transform", (d, i) => `translate(0,${i * 45})`);
+
+    legends_items.append("rect")
+        .attr("fill", d => color(d))
+        .attr("width", 40)
+        .attr("height", 40)
+        .attr("class", d => d);
+
+    // Only one category filtered at a time
+    let activeCategory = null;
+
+    ////////////////////////////////////////////////////////////////////////
+    // Missing Part 5 : clicking legend filters points
+    ////////////////////////////////////////////////////////////////////////
+    legends_items.on("click", function (event, d) {
+        if (activeCategory === d) {
+            // clicking again resets
+            activeCategory = null;
+            d3.selectAll("circle").classed("legendSelect", false);
+        } else {
+            activeCategory = d;
+            d3.selectAll("circle").classed("legendSelect", p => p[colorCol] !== d);
+        }
+    });
+
+    legends_items.append("text")
+        .text(d => d)
+        .attr("dx", 45)
+        .attr("dy", 25)
+        .attr("class", "legend")
+        .attr("fill", "black");
+}
